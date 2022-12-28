2bb56d73d6b43d0404f625bfd409a51ac54a84ce
Jarrod Lowe <github@rrod.net>
2022-12-28 19:30:09 +0000

    Add a lambda invoke provider method

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -25,6 +25,9 @@ import (
 	"time"
 
 	"github.com/golang-jwt/jwt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/lambda"
 )
 
 type ProviderCreator func(user, password string) (Provider, error)
@@ -55,6 +58,16 @@ type StaticCredentials struct {
 	User     string
 	Password string
 	S3Bucket *S3Bucket
+}
+
+type LambdaPayload struct {
+	User     string
+	Password string
+}
+
+type LambdaResponse struct {
+	StatusCode int
+	S3Bucket
 }
 
 func NewStaticCredentialsProviderCreator(staticCreds StaticCredentials) ProviderCreator {
@@ -119,5 +132,44 @@ func NewHTTPBasicAuthProviderCreator(timeout time.Duration, url string) Provider
 			return nil, err
 		}
 		return newS3Provider(bucket)
+	}
+}
+
+func NewLambdaProviderCreator(timeout time.Duration, function string) ProviderCreator {
+	sess, err := session.NewSession() // TODO: Args for key?
+	if err != nil {
+		panic(err)
+	}
+	return func(user, password string) (Provider, error) {
+		client := lambda.New(sess)
+		payload := LambdaPayload{
+			User:     user,
+			Password: password,
+		}
+
+		payloadJson, err := json.Marshal(payload)
+		if err != nil {
+			return nil, err
+		}
+
+		res, err := client.Invoke(&lambda.InvokeInput{
+			FunctionName:   aws.String(function),
+			InvocationType: aws.String("RequestResponse"),
+			Payload:        payload,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		var lambdaResponse LambdaResponse
+		if err := json.NewDecoder(res.Payload).Decode(&lambdaResponse); err != nil {
+			return nil, err
+		}
+
+		if lambdaResponse.StatusCode != 200 {
+			return nil, fmt.Errorf("received status code %v", lambdaResponse.StatusCode)
+		}
+
+		return new S3Provider(lambdaResponse)
 	}
 }
